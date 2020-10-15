eab53d16b1f6bbab75460bad83be4aa066f7c9a4
Phunguyen <yolosolo25@example.com>
2020-10-15 18:36:20 +0700

    Update SelectionSort.java

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,9 +1,13 @@
 package phunguyen;
 
 public class SelectionSort {
-	int indexMin;
-	//duyet qua tat ca cac so
-	for(int i =0; i < arr.length -1; i++) {
-		
+	public void selectionSort(int arr[]) {
+		int indexMin;
+		// duyet qua tat ca cac so
+		for(int i =0; i < arr.length -1; i++) {
+			// cai dat phan tu hien tai la min
+			indexMin = i;
+		}
 	}
+	
 }
